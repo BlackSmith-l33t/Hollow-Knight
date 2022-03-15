656e8fb5f5cadc108ebd69e5d37e2d9140a54ab5
DESKTOP-NK217EO\Melchizedek <nobody133738@gmail.com>
2022-03-15 10:38:00 +0900

    기본 틀 수정

---
+++ winAPI_2D_imitation/winAPI_2D_imitation/CCameraManager.cpp
@@ -0,0 +1,169 @@
+#include "framework.h"
+#include "CCameraManager.h"
+#include "CGameObject.h"
+#include "CTexture.h"
+
+CCameraManager::CCameraManager()
+{
+	m_fptLookAt = fPoint(WINSIZEX / 2.f, WINSIZEY / 2.f);
+	m_fptCurLookAt = m_fptLookAt;
+	m_fptPrevLookAt = m_fptLookAt;
+	m_pTargetObj = nullptr;
+	m_fAccTime = m_fTime;
+	m_fSpeed = 0;
+
+	m_eEffect = CAM_EFFECT::NONE;
+	m_pTex = nullptr;
+	m_fEffectDuration = 0.f;
+	m_fCurTime = 0.f;
+}
+
+CCameraManager::~CCameraManager()
+{
+
+}
+
+void CCameraManager::init()
+{
+	m_pTex = CResourceManager::getInst()->CreateTexture(L"CameraTex", WINSIZEX, WINSIZEY);
+}
+
+void CCameraManager::update()
+{
+	if (m_pTargetObj)
+	{
+		if (m_pTargetObj->isDead())
+		{
+			m_pTargetObj = nullptr;
+		}
+		else
+		{
+			SetLookAt(m_pTargetObj->GetPos());
+		}
+	}
+
+	// 화면 중앙과 카메라 LookAt 좌표 사이의 차이 계산
+	CalDiff();
+}
+
+void CCameraManager::render(HDC hDC)
+{
+	if (CAM_EFFECT::NONE == m_eEffect)
+		return;
+
+	m_fCurTime += fDT;
+	if (m_fEffectDuration < m_fCurTime)
+	{
+		m_eEffect = CAM_EFFECT::NONE;
+		return;
+	}
+
+	float fRatio = 0.f;
+	int iAlpha = 0;
+	fRatio = m_fCurTime / m_fEffectDuration;
+	if (CAM_EFFECT::FADE_OUT == m_eEffect)
+	{
+		iAlpha = (int)(255.f * fRatio);
+	}
+	else if (CAM_EFFECT::FADE_IN == m_eEffect)
+	{
+		iAlpha = (int)(255.f * (1.f - fRatio));
+	}
+
+	BLENDFUNCTION bf = {};
+
+	bf.BlendOp = AC_SRC_OVER;
+	bf.BlendFlags = 0;
+	bf.AlphaFormat = 0;
+	bf.SourceConstantAlpha = iAlpha;
+
+	AlphaBlend(hDC
+		, 0, 0
+		, (int)(m_pTex->GetBmpWidth())
+		, (int)(m_pTex->GetBmpHeight())
+		, m_pTex->GetDC()
+		, 0, 0
+		, (int)(m_pTex->GetBmpWidth())
+		, (int)(m_pTex->GetBmpHeight())
+		, bf);
+}
+
+void CCameraManager::SetLookAt(fPoint lookAt)
+{
+	m_fptLookAt = lookAt;
+	float fMoveDist = (m_fptLookAt - m_fptPrevLookAt).Length();
+
+	m_fSpeed = fMoveDist / m_fTime;
+	m_fAccTime = 0.f;
+}
+
+void CCameraManager::SetTargetObj(CGameObject* target)
+{
+	m_pTargetObj = target;
+}
+
+fPoint CCameraManager::GetLookAt()
+{
+	return m_fptCurLookAt;
+}
+
+fPoint CCameraManager::GetRenderPos(fPoint objPos)
+{
+	return objPos - m_fptDiff;
+}
+
+fPoint CCameraManager::GetRealPos(fPoint renderPos)
+{
+	// 렌더링 좌표에서 차이값만큼 더해주면 절대 좌표가 나옴.
+	return renderPos + m_fptDiff;
+}
+
+void CCameraManager::FadeIn(float duration)
+{
+	m_eEffect = CAM_EFFECT::FADE_IN;
+	m_fEffectDuration = duration;
+
+	if (0.f == m_fEffectDuration)
+	{
+		assert(nullptr);
+	}
+}
+
+void CCameraManager::FadeOut(float duration)
+{
+	m_eEffect = CAM_EFFECT::FADE_OUT;
+	m_fEffectDuration = duration;
+
+	if (0.f == m_fEffectDuration)
+	{
+		assert(nullptr);
+	}
+}
+
+void CCameraManager::Scroll(fVec2 vec, float velocity)
+{
+	m_fptLookAt = m_fptLookAt + vec * velocity * fDT;
+	m_fptCurLookAt = m_fptCurLookAt + vec * velocity * fDT;
+
+	fPoint fptCenter = fPoint(WINSIZEX / 2.f, WINSIZEY / 2.f);
+	m_fptDiff = m_fptCurLookAt - fptCenter;
+}
+
+void CCameraManager::CalDiff()
+{
+	m_fAccTime += fDT;
+
+	// 시간이 지나면, 도착한것으로 간주
+	if (m_fTime <= m_fAccTime)
+	{
+		m_fptCurLookAt = m_fptLookAt;
+	}
+	else
+	{
+		fPoint fptCenter = fPoint(WINSIZEX / 2.f, WINSIZEY / 2.f);
+
+		m_fptCurLookAt = m_fptPrevLookAt + (m_fptLookAt - m_fptPrevLookAt).normalize() * m_fSpeed * fDT;
+		m_fptDiff = m_fptCurLookAt - fptCenter;
+		m_fptPrevLookAt = m_fptCurLookAt;
+	}
+}
---
+++ winAPI_2D_imitation/winAPI_2D_imitation/CResourceManager.cpp
@@ -0,0 +1,116 @@
+#include "framework.h"
+#include "CResourceManager.h"
+#include "CTexture.h"
+#include "CSound.h"
+
+CResourceManager::CResourceManager()
+{
+	m_mapTex = {};
+}
+
+CResourceManager::~CResourceManager()
+{
+	// 자료구조에 저장된 모든 Texture 삭제
+	for (map<wstring, CTexture*>::iterator iter = m_mapTex.begin(); iter != m_mapTex.end(); iter++)
+	{
+		if (nullptr != iter->second)
+		{
+			delete iter->second;
+		}
+	}
+	m_mapTex.clear();
+
+	for (map<wstring, CSound*>::iterator iter = m_mapSound.begin(); iter != m_mapSound.end(); iter++)
+	{
+		// TODO : 사운드 삭제 진행
+	}
+}
+
+CTexture* CResourceManager::FindTexture(const wstring& strKey)
+{
+	// Texture 키 값을 통해 탐색
+	map<wstring, CTexture*>::iterator iter = m_mapTex.find(strKey);
+	
+	if (m_mapTex.end() == iter)
+	{
+		return nullptr;
+	}
+
+	return iter->second;
+}
+
+CTexture* CResourceManager::LoadTextrue(const wstring& strKey, const wstring& strRelativePath)
+{
+	// Texture를 불러오기 전 자료구조에 이미 Texture가 있는지 확인
+	CTexture* pTex = FindTexture(strKey);
+	if (nullptr != pTex)
+	{
+		return pTex;
+	}
+	
+	// Texture 저장 경로 확인
+	wstring strFilePath = CPathManager::getInst()->GetContentPath();
+	strFilePath += strRelativePath;
+
+	// Texture 생성 후 저장
+	pTex = new CTexture;
+	pTex->Load(strFilePath);
+	pTex->SetKey(strKey);
+	pTex->SetRelativePath(strRelativePath);
+
+	m_mapTex.insert(make_pair(strKey, pTex));
+	
+	return pTex;
+}
+
+CTexture* CResourceManager::CreateTexture(const wstring& strKey, UINT width, UINT height)
+{
+	CTexture* pTex = FindTexture(strKey);
+	if (nullptr != pTex)
+	{
+		return nullptr;
+	}
+
+	pTex = new CTexture();
+	pTex->Create(width, height);
+	pTex->SetKey(strKey);
+
+	m_mapTex.insert(make_pair(strKey, pTex));
+	return pTex;
+}
+
+CSound* CResourceManager::FindSound(const wstring& strKey)
+{
+	map<wstring, CSound*>::iterator iter = m_mapSound.find(strKey);
+
+	if (m_mapSound.end() == iter)
+	{
+		return nullptr;
+	}
+
+	return iter->second;
+}
+
+CSound* CResourceManager::LoadSound(const wstring& strKey, const wstring& strRelativePath)
+{
+	// Texture를 불러오기 전 자료구조에 이미 Texture가 있는지 확인
+	CSound* pSound = FindSound(strKey);
+	if (nullptr != pSound)
+	{
+		return pSound;
+	}
+
+	// Texture 저장 경로 확인
+	wstring strFilePath = CPathManager::getInst()->GetContentRelativePath();
+	strFilePath += strRelativePath;
+
+	// Texture 생성 후 저장
+	pSound = new CSound;
+	pSound->Load(strFilePath);
+	pSound->SetKey(strKey);
+	pSound->SetRelativePath(strRelativePath);
+
+	m_mapSound.insert(make_pair(strKey, pSound));
+
+	return pSound;
+}
